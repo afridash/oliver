2824246384aa834b9e4f65cabbcb3a1082b4c3dc
Richard Igbiriki <imorobebh@gmail.com>
2017-08-31 11:12:29 +0100

    Showing total number of added questions on home page

--- oliver/src/App.js
+++ oliver/src/App.js
@@ -3,8 +3,27 @@ import {Button, Navbar, NavItem, Nav} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import oliver from './oliver.jpg'
 import './App.css'
-
+import {Firebase} from './auth/firebase'
+const firebase =  require('firebase')
 class App extends Component {
+  constructor (props){
+    super (props)
+    this.state ={
+      questions:"We are counting..."
+    }
+    this.ref = firebase.database().ref().child('questions')
+  }
+  async componentWillMount (){
+    this.totalQuestions = 0
+       this.ref.once('value', (snapshots)=>{
+      snapshots.forEach((Datasnapshots)=>{
+        Datasnapshots.forEach((data)=>{
+          this.totalQuestions += 1
+          this.setState({questions:this.totalQuestions})
+        })
+      })
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -17,6 +36,7 @@ class App extends Component {
         </p>
         <Link to="/questions"><Button bsStyle="primary" bsSize="large">Add Questions</Button></Link>
         <div style={{margin:5}}>Or Maybe...View Them <Link to="/view">Here</Link></div>
+        <div style={{marginTop:'10%'}}><h3 className='text-info'>Our Questions so Far: {this.state.questions}</h3></div>
         <Navbar fixedBottom >
           <Navbar.Header>
             <Navbar.Brand>
