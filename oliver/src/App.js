ff8f35455cef83c8f1b8eff3cd20d70c81240785
Richard Igbiriki <imorobebh@gmail.com>
2017-09-13 10:29:50 +0100

    Confetti celebration for milestones

--- oliver/src/App.js
+++ oliver/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {Button, Navbar, NavItem, Nav} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import oliver from './oliver.jpg'
-import './App.css'
+import './AppStyles.css'
 import {Firebase} from './auth/firebase'
 const firebase =  require('firebase')
 class App extends Component {
@@ -12,6 +12,7 @@ class App extends Component {
       questions:"We are counting..."
     }
     this.ref = firebase.database().ref().child('questions')
+    this.celRef = firebase.database().ref().child('celebration')
   }
   async componentWillMount (){
     this.totalQuestions = 0
@@ -23,6 +24,98 @@ class App extends Component {
         })
       })
     })
+    this.celRef.once('value', (cel)=>{
+      this.setState(cel.val())
+    })
+  }
+  showConfetti () {
+    return (
+      <div>
+        <div className="confetti confetti1"></div>
+        <div className="confetti confetti2"></div>
+        <div className="confetti confetti3"></div>
+        <div className="confetti confetti4"></div>
+        <div className="confetti confetti5"></div>
+        <div className="confetti confetti6"></div>
+        <div className="confetti confetti7"></div>
+        <div className="confetti confetti8"></div>
+        <div className="confetti confetti9"></div>
+        <div className="confetti confetti10"></div>
+        <div className="confetti confetti11"></div>
+        <div className="confetti confetti12"></div>
+        <div className="confetti confetti13"></div>
+        <div className="confetti confetti14"></div>
+        <div className="confetti confetti15"></div>
+        <div className="confetti confetti16"></div>
+        <div className="confetti confetti17"></div>
+        <div className="confetti confetti18"></div>
+        <div className="confetti confetti19"></div>
+        <div className="confetti confetti20"></div>
+        <div className="confetti confetti21"></div>
+        <div className="confetti confetti1"></div>
+        <div className="confetti confetti2"></div>
+        <div className="confetti confetti3"></div>
+        <div className="confetti confetti4"></div>
+        <div className="confetti confetti5"></div>
+        <div className="confetti confetti6"></div>
+        <div className="confetti confetti7"></div>
+        <div className="confetti confetti8"></div>
+        <div className="confetti confetti9"></div>
+        <div className="confetti confetti10"></div>
+        <div className="confetti confetti11"></div>
+        <div className="confetti confetti12"></div>
+        <div className="confetti confetti13"></div>
+        <div className="confetti confetti14"></div>
+        <div className="confetti confetti15"></div>
+        <div className="confetti confetti16"></div>
+        <div className="confetti confetti17"></div>
+        <div className="confetti confetti18"></div>
+        <div className="confetti confetti19"></div>
+        <div className="confetti confetti20"></div>
+        <div className="confetti confetti21"></div>
+
+        <div className="confetti confetti22"></div>
+        <div className="confetti confetti23"></div>
+        <div className="confetti confetti24"></div>
+        <div className="confetti confetti25"></div>
+        <div className="confetti confetti26"></div>
+        <div className="confetti confetti27"></div>
+        <div className="confetti confetti28"></div>
+        <div className="confetti confetti29"></div>
+        <div className="confetti confetti30"></div>
+        <div className="confetti confetti31"></div>
+        <div className="confetti confetti32"></div>
+        <div className="confetti confetti33"></div>
+        <div className="confetti confetti34"></div>
+        <div className="confetti confetti35"></div>
+        <div className="confetti confetti36"></div>
+        <div className="confetti confetti37"></div>
+        <div className="confetti confetti38"></div>
+        <div className="confetti confetti39"></div>
+        <div className="confetti confetti40"></div>
+        <div className="confetti confetti41"></div>
+        <div className="confetti confetti22"></div>
+        <div className="confetti confetti23"></div>
+        <div className="confetti confetti24"></div>
+        <div className="confetti confetti25"></div>
+        <div className="confetti confetti26"></div>
+        <div className="confetti confetti27"></div>
+        <div className="confetti confetti28"></div>
+        <div className="confetti confetti29"></div>
+        <div className="confetti confetti30"></div>
+        <div className="confetti confetti31"></div>
+        <div className="confetti confetti32"></div>
+        <div className="confetti confetti33"></div>
+        <div className="confetti confetti34"></div>
+        <div className="confetti confetti35"></div>
+        <div className="confetti confetti36"></div>
+        <div className="confetti confetti37"></div>
+        <div className="confetti confetti38"></div>
+        <div className="confetti confetti39"></div>
+        <div className="confetti confetti40"></div>
+        <div className="confetti confetti41"></div>
+      </div>
+    )
   }
   render() {
     return (
@@ -49,6 +142,7 @@ class App extends Component {
       </Nav>
     </Navbar.Collapse>
         </Navbar>
+        {this.state.questions > this.state.start && this.state.questions < this.state.end && this.showConfetti()}
       </div>
     );
   }
