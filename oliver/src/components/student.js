fe990b8b153d0fe961263a40111fe54a042a4d94
Richard Igbiriki <imorobebh@gmail.com>
2017-11-22 16:50:42 +0100

    charts for sessions and activities added

--- oliver/src/components/student.js
+++ oliver/src/components/student.js
@@ -14,15 +14,21 @@ class Student extends Component {
       userId:'',
       username:'',
       courseId:'',
+      data:[{x:'', y:0}],
+      activities:[{x:'', y:0}],
+      start:0,
+      end:2,
     }
     this.userId = this.props.match.params.id
     this.courseId = this.props.match.params.courseId
     this.statRef = firebase.database().ref().child('student_stats')
     this.studentRef = firebase.database().ref().child('users')
     this.sessionRef = firebase.database().ref().child('sessions')
+    this.activitiesRef = firebase.database().ref().child('course_activities').child(this.courseId)
+    this.data = []
   }
 
-  componentWillMount () {
+  async componentWillMount () {
      this.statRef.child(this.courseId).child(this.userId).once('value', (snapshots)=>{
         snapshots.forEach((snapshot)=>{
          this.setState({[snapshot.key]:snapshot.val()})
@@ -33,21 +39,26 @@ class Student extends Component {
        this.setState({displayName:snapshot.val().firstName + " " + snapshot.val().lastName, last_seen:snapshot.val().last_seen})
      })
 
-     this.sessionRef.child(this.userId).once('value', (snapshots)=>{
+     await this.sessionRef.child(this.userId).once('value', (snapshots)=>{
+       this.data = []
         snapshots.forEach((date)=>{
+          var time = 0
          date.forEach((session)=>{
-           console.log('Time:', session.val().time_difference)
+          time += session.val().time_difference
          })
+         this.data.push({x: moment(date.key).format('ddd MMM Do YYYY'), y:time/60})
+         this.setState({data:this.data})
        })
      })
 
-     this.sessionRef.child(this.userId).orderByKey().limitToLast(2).once('value', (snapshots)=>{
-        snapshots.forEach((date)=>{
-           console.log(moment(date.key).format('ddd MMM Do YYYY'))
+     await this.activitiesRef.child(this.userId).once('value', (snapshots)=>{
+       this.activities = []
+        snapshots.forEach((activity)=>{
+         this.activities.push({x: moment(activity.val().createdAt).format('LT'), y:activity.val().percentage})
+         this.setState({activities:this.activities})
        })
      })
    }
-
   mouseOverHandler = (d, e) => {
      var top = `${e.screenY - 10}px`
      var left =  `${e.screenX + 10}px`
@@ -130,7 +141,7 @@ class Student extends Component {
     axes
     grid
     colorBars
-    axisLabels={{x: 'Session (Weekly)', y: 'Time (Sec)'}}
+    axisLabels={{x: 'Session (Weekly)', y: 'Time (Mins)'}}
     yAxisOrientLeft
     //xDomainRange={['0', 'sessionDate']}
     //yDomainRange={[0, 5000]}
@@ -138,19 +149,29 @@ class Student extends Component {
     height={250}
     width={550}
     margin={{top: 40, right: 50, bottom: 50, left: 60}}
-    data={[
-          { x: 'Monday', y: 20 },
-          { x: 'Tuesday', y: 30 },
-          { x: 'Wednesday', y: 40 },
-          { x: 'Thursday', y: 30 },
-          { x: 'Friday', y: 30 },
-          { x: 'Saturday', y: 20 },
-          { x: 'Sunday', y: 6 }
-    ]}
+    data={this.state.data}
     mouseOverHandler={this.mouseOverHandler}
     mouseOutHandler={this.mouseOutHandler}
     mouseMoveHandler={this.mouseMoveHandler}
   />
+
+  <BarChart
+  axes
+  grid
+  colorBars
+  axisLabels={{x: 'Attempts', y: 'Scores (%)'}}
+  yAxisOrientLeft
+  //xDomainRange={['0', 'sessionDate']}
+  //yDomainRange={[0, 5000]}
+  //datePattern="%d-%b-%y %H:%M"
+  height={250}
+  width={550}
+  margin={{top: 40, right: 50, bottom: 50, left: 60}}
+  data={this.state.activities}
+  mouseOverHandler={this.mouseOverHandler}
+  mouseOutHandler={this.mouseOutHandler}
+  mouseMoveHandler={this.mouseMoveHandler}
+/>
 
     </div>
     	</div>
